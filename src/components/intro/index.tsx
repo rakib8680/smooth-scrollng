1397fae8e5ec78d12f5d490f6334a1b1a3e7c3fe
rakib8680 <rakib.cst.1st@gmail.com>
2025-04-23 19:36:01 +0600

    Add GSAP animation in intro section

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,17 +1,53 @@
 import Image from "next/image";
 import styles from "./style.module.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
 
 const Index = () => {
+  const backgroundImage = useRef(null);
+  const introImage = useRef(null);
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: "top",
+        end: "+=500px",
+        scrub: true,
+        // markers: true,
+      },
+    });
+    timeline
+      .from(backgroundImage.current, {
+        clipPath: "inset(15%)",
+      })
+      .to(
+        introImage.current,
+        {
+          height: "200px",
+        },
+        0
+      );
+  }, []);
+
   return (
     <div className={styles.intro}>
-      <div className={styles.backgroundImage}>
+      <div ref={backgroundImage} className={styles.backgroundImage}>
         <Image src={"/images/img1.jpg"} fill={true} alt="background" />
       </div>
       <div className={styles.introContainer}>
-        <div data-scroll data-scroll-speed="0.3" className={styles.introImage}>
+        <div
+          ref={introImage}
+          data-scroll
+          data-scroll-speed="0.3"
+          className={styles.introImage}
+        >
           <Image src={"/images/img3.jpg"} fill={true} alt="intro" />
         </div>
-        <h1 data-scroll data-scroll-speed="0.4">Smooth Scroll</h1>
+        <h1 data-scroll data-scroll-speed="0.4">
+          Smooth Scroll
+        </h1>
       </div>
     </div>
   );
